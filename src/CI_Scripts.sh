aba845c34a9a3bf3fe4c8d7a7c528f05e9e8b9b1
arkady <zvezdoreel@gmail.com>
2022-07-25 07:02:38 -0700

    junit# Please enter the commit message for your changes. Lines starting

--- src/CI_Scripts.sh
+++ src/CI_Scripts.sh
@@ -1,3 +1,3 @@
-python -m pytest /proj/Test/*
+python -m pytest -v --junitxml="unit.xml" /proj/Test/*
 python -m black /proj
-python -m pylint /proj
+python -m pylint --output-format=pylint_junit.JUnitReporter /proj >> ./lint.xml
---
+++ src/integration_tests/integrationtest.ps1
@@ -0,0 +1,3 @@
+﻿$resp = curl -UseBasicParsing http://127.0.0.1:5000
+if ($resp.StatusCode -eq 200 -and $resp.Content -eq "Hello World!"){ "good"}
+else{"error"}
